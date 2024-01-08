792bce44871ff4b64454099ab4498aa524b00c9c
Randa-10 <randa010011012@gmail.com>
2024-01-08 22:43:55 +0200

    movies details card

--- src/app/components/movie-details/movie-details.component.ts
+++ src/app/components/movie-details/movie-details.component.ts
@@ -41,31 +41,34 @@
 //     )
 // }
 // }
-import { Component, Input, OnInit, inject } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges, inject } from '@angular/core';
 import { DescriptionPipe } from './../../shared/pipe/description.pipe';
 import { MoviesService } from '../../shared/services/movies.service';
 import { IVideoContent } from './../../shared/models/ivideo-content';
 import { ActivatedRoute } from '@angular/router';
 import { ImgPipe } from "../../shared/pipe/img.pipe";
 import { Router } from 'express';
+import { HeaderComponent } from '../header/header.component';
+import { DomSanitizer } from '@angular/platform-browser';
+import { NgIf } from '@angular/common';
 
 @Component({
     selector: 'app-movie-details',
     standalone: true,
     templateUrl: './movie-details.component.html',
     styleUrls: ['./movie-details.component.scss'],
-    imports: [DescriptionPipe, ImgPipe]
+    imports: [DescriptionPipe, ImgPipe,HeaderComponent,NgIf]
 })
 export class MovieDetailsComponent implements OnInit {
-  // @Input({ required: true }) bannerTitle = '';
-  // @Input() bannerOverview = '';
+  userProfileImg!: string;
   movieService: MoviesService;
   movieService2: ActivatedRoute;
   currentdetai: number = 0;
   movei!: IVideoContent | undefined;
-  // currentdetai:number=0
   productsIDSList:number[]=[];
   currentPrdIndex:number=0;
+  @Input() key='r_pUE7OcN8w';
+
 //  private router=inject(Router)
   constructor(private moviesService: MoviesService, private activatedRoute: ActivatedRoute ) {
     this.movieService = moviesService;
@@ -99,4 +102,20 @@ export class MovieDetailsComponent implements OnInit {
   //   this.currentPrdIndex=this.productsIDSList.indexOf(this.currentdetai);
   //   this.router.navigate(['/details/:id',this.productsIDSList[++this.currentPrdIndex]])
   // }
+
+
+  // Sample data, replace with actual movie details
+  private sanitizer=inject(DomSanitizer)
+
+  videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.key}?autoplay=1&mute=1&loop=1&controls=0`);
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes ['Key']){
+      this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.key}?autoplay=1&mute=1&loop=1&controls=0`);
+    }
+  }
+  showTrailer: boolean = false;
+
+  toggleTrailer(): void {
+    this.showTrailer = !this.showTrailer;
+  }
 }
